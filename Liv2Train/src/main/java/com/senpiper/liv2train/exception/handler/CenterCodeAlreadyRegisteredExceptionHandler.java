feaeb0a3d462cf0b4e335ad3490464a9a5e80e03
Ravi Ranjan <95235883+celestial-101@users.noreply.github.com>
2022-03-17 14:24:03 +0530

    Update and rename EmailAlreadyRegisteredExceptionHandler.java to CenterCodeAlreadyRegisteredExceptionHandler.java

--- Liv2Train/src/main/java/com/senpiper/liv2train/exception/handler/CenterCodeAlreadyRegisteredExceptionHandler.java
+++ Liv2Train/src/main/java/com/senpiper/liv2train/exception/handler/CenterCodeAlreadyRegisteredExceptionHandler.java
@@ -8,10 +8,10 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @RestControllerAdvice
 @EnableWebMvc
-public class EmailAlreadyRegisteredExceptionHandler {
-	@ExceptionHandler(value = { EmailAlreadyRegisterException.class })
+public class CenterCodeAlreadyRegisteredExceptionHandler {
+	@ExceptionHandler(value = { CenterCodeAlreadyRegisterException.class })
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public InvalidExceptionResponse exceptionHandler(Exception ex) {
-    	return new InvalidExceptionResponse(400,"Email Already Registered");
+    	return new InvalidExceptionResponse(600,"Center Code Already Registered");
     }
 }
